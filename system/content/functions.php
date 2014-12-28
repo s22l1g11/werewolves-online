0e54d3a1f8fe749ed7fbd1f18af783d2405c9383
s22l1g11 <s22l1g11@gmail.com>
2014-12-28 18:20:01 +0100

    added new functions

--- system/content/functions.php
+++ system/content/functions.php
@@ -7,4 +7,20 @@ function establishConnection()
 	return new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
 }
 
+function get_user_data($requestedInfo)
+{
+	if ($requestedInfo == "uid")
+	{
+		return $_SESSION['userData']['uid'];
+	}
+	if ($requestedInfo == "email")
+	{
+		return $_SESSION['userData']['email'];
+	}
+	if ($requestedInfo == 'nickname')
+	{
+		return $_SESSION['userData']['nickname'];
+	}
+}
+
 ?>
