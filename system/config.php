6a17cdadeb7fbad3803a02b3c312c00ee7312eaf
s22l1g11 <s22l1g11@gmail.com>
2014-12-28 17:56:11 +0100

    edit prefix style "[…]_"

--- system/config.php
+++ system/config.php
@@ -4,6 +4,6 @@ define("DB_HOST", "localhost");
 define("DB_NAME", "name");
 define("DB_USER", "user");
 define("DB_PASSWORD", "password");
-define("DB_PREFIX", "prefix");
+define("DB_PREFIX", "prefix_");
 
 ?>
