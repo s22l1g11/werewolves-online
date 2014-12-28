1991006593c53ff920c5319c29bb1edbf3da47a2
s22l1g11 <s22l1g11@gmail.com>
2014-12-28 17:56:18 +0100

    added login

--- system/content/content.php
+++ system/content/content.php
@@ -19,11 +19,43 @@ class Content
 			 * 			 
 			 */
 		}
-		else if ($_SESSION['userOnline'] === false)
+		else if ($_SESSION['userOnline'] === false && $_GET['action'] !== "login")
 		{
 			require_once('html/login.html');	
 		}
+		else
+		{
+			$this->login();			
+		}
 		
+	}
+	
+	private function login()
+	{
+		// establish connection
+			$db = establishConnection();
+			// create query
+			$query = "SELECT * FROM ".DB_PREFIX."users WHERE email LIKE :mail";
+			// prepare
+			$stmt = $db->prepare($query);
+			// bind params
+			$stmt->bindParam(":mail", $_POST['email']);
+			// execute
+			if ($stmt->execute())
+			{
+				// fetch
+				$result = $stmt->fetchAll();
+				// check
+				if ($result[0]['email'] === $_POST['email'] && password_verify($_POST['password'], $result[0]['password']))
+				{
+					$_SESSION['userOnline'] = true;
+					$_SESSION['userData'] = array("uid" => $result[0]['uid'], "email" => $result[0]['email'], "nickname" => $result[0]['nickname']); 
+					require_once('html/loginSuccessful.html');
+				}
+				else {
+					require_once('html/loginError.html');
+				}
+			}
 	}
 }
 
